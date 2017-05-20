c929cb4ea2bd66b1b19f3508fb14a53f84812692
Fil <Fil@DESKTOP-MBK3S05>
2017-05-20 18:51:51 -0500

    dataset

--- glcm_git/main.cpp
+++ glcm_git/main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 using namespace cv;
 
 #include "App.h"
-//#include "AppGlcm.h"
+#include "AppGlcm.h"
 
 int main() {
 
-	/*App* app = getApplication();
+	App* app = getApplication();
 	app->setNameBD("in.data");
 	app->setNameResults("out.data");
 	app->initProcessing();
 	app->offLineProcessing();
 	app->onLineProcessing();
 	app->endProcessing();
-*/
+
 
 	/*Mat mat = imread("img.jpg");
 	imshow("xd", mat);
